4332dd7a914d02e7fd9304cff31c24c8f171f0ef
António Gonçalves <acgtwentyone@gmail.com>
2022-04-08 22:35:16 -0100

    Update ErrorMessage.js

--- app/components/ErrorMessage.js
+++ app/components/ErrorMessage.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import {Text} from 'native-base';
-import {StyleSheet} from 'react-native';
 
-const ErrorMessage = ({style, props, name, message, errors}) => (
+const ErrorMessage = ({props, name, errors}) => (
   <>
-    {errors[name] && (
-      <Text style={[styles.container, {...style}]} {...props}>
-        {message}
-      </Text>
-    )}
+    <Text color="red.100" {...props}>
+      {errors[name]?.message}
+    </Text>
   </>
 );
-
-const styles = StyleSheet.create({
-  container: {},
-});
 
 export default React.memo(ErrorMessage);
