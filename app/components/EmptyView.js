fefad7c6f7a04a6b8b31a7a0868ffdcb640f5760
António Gonçalves <acgtwentyone@gmail.com>
2022-04-13 21:59:18 -0100

    Update EmptyView.js

--- app/components/EmptyView.js
+++ app/components/EmptyView.js
@@ -2,10 +2,16 @@ import {Box, Icon, Text} from 'native-base';
 import React from 'react';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const EmptyView = ({props, title}) => {
+const EmptyView = ({iconName, isIcon = true, props, title}) => {
   return (
-    <Box p={5} alignItems="center" flex={1} {...props}>
-      <Icon as={MaterialCommunityIcons} name="database-search" size={16} />
+    <Box p={5} alignItems="center" flex={1} {...props} justifyContent="center">
+      {isIcon && (
+        <Icon
+          as={MaterialCommunityIcons}
+          name={iconName ? iconName : 'database-search'}
+          size={16}
+        />
+      )}
       <Text m={2}>{title ? title : 'Não há registros ...'}</Text>
     </Box>
   );
