1eb2cf166395144547fb2f0c461c936e45807eef
António Gonçalves <acgtwentyone@gmail.com>
2022-04-13 21:59:26 -0100

    Update HomeScreen.js

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -5,9 +5,10 @@ import {
   BookItem,
   EmptyView,
   ListTitle,
+  PageItem,
   Screen,
 } from '../components';
-import {useLoadBooks} from '../hooks';
+import {useLoadBooks, useLoadPages} from '../hooks';
 
 const HomeScreen = () => {
   const RecentBooks = ({isFavScreen, title}) => {
@@ -23,7 +24,44 @@ const HomeScreen = () => {
           )}
           keyExtractor={item => item.id.toString()}
           horizontal={true}
-          ListHeaderComponent={books.length === 0 && !loading && <EmptyView />}
+          ListHeaderComponent={
+            books.length === 0 &&
+            !loading && (
+              <EmptyView
+                iconName="cloud-off-outline"
+                title={
+                  isFavScreen
+                    ? 'Não há livros recentes'
+                    : 'Não há livros adicionados aos favoritos'
+                }
+              />
+            )
+          }
+        />
+      </Box>
+    );
+  };
+
+  const RecentPages = () => {
+    const {pages, loading} = useLoadPages();
+    return (
+      <Box pb={4}>
+        {<ListTitle title="Recent pages" />}
+        {(!pages || pages.length === 0) && loading && <AppActivityIndicator />}
+        <FlatList
+          data={pages}
+          renderItem={({item}) => <PageItem item={item} recent={true} />}
+          keyExtractor={item => item.id.toString()}
+          horizontal={true}
+          ListHeaderComponent={
+            pages.length === 0 &&
+            !loading && (
+              <EmptyView
+                iconName="cloud-off-outline"
+                title="Não há páginas recentes"
+              />
+            )
+          }
         />
       </Box>
     );
@@ -31,8 +69,16 @@ const HomeScreen = () => {
 
   return (
     <Screen>
-      <RecentBooks title="Recent books" isFavScreen={false} />
-      <RecentBooks title="Recent favourities books" isFavScreen={true} />
+      <FlatList
+        ListHeaderComponent={
+          <>
+            <RecentBooks title="Recent books" isFavScreen={false} />
+            <RecentPages />
+            <RecentBooks title="Recent favourities books" isFavScreen={true} />
+          </>
+        }
+        keyExtractor={item => item.toString()}
+      />
     </Screen>
   );
 };
