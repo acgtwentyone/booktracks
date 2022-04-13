8c91a9d802e5dbb90dfdc6a60ffbf13034f9085f
António Gonçalves <acgtwentyone@gmail.com>
2022-04-13 20:09:09 -0100

    Create index.js

---
+++ app/hooks/index.js
@@ -0,0 +1,6 @@
+export {default as useIsLoggedIn} from './useIsLoggedIn';
+export {default as useLoadBooks} from './useLoadBooks';
+export {default as useLoadPages} from './useLoadPages';
+export {default as useTabScreenOptions} from './useTabScreenOptions';
+export {default as useScreenOptions} from './useScreenOptions';
+export {default as useShowMessage} from './useShowMessage';
