0465ce3e9f70877835c132f1ebd094b1fa1ad8a4
António Gonçalves <acgtwentyone@gmail.com>
2022-04-13 20:33:50 -0100

    Update HomeScreen.js

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import {Box, FlatList} from 'native-base';
-import {BookItem, ListTitle, Screen} from '../components';
+import {Box, FlatList, Icon, Text} from 'native-base';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {AppActivityIndicator, BookItem, ListTitle, Screen} from '../components';
 import {useLoadBooks} from '../hooks';
 
 const HomeScreen = () => {
+  const EmptyView = () => (
+    <Box p={5} alignItems="center" flex={1}>
+      <Icon as={MaterialCommunityIcons} name="database-search" size={16} />
+      <Text m={2}>Não há registros ...</Text>
+    </Box>
+  );
+
   const RecentBooks = ({isFavScreen, title}) => {
-    const {books} = useLoadBooks(isFavScreen);
+    const {books, loading} = useLoadBooks(isFavScreen);
     return (
       <Box pb={4}>
-        <ListTitle title={title} />
+        {<ListTitle title={title} />}
+        {(!books || books.length === 0) && loading && <AppActivityIndicator />}
         <FlatList
           data={books}
           renderItem={({item}) => (
@@ -16,6 +25,7 @@ const HomeScreen = () => {
           )}
           keyExtractor={item => item.id.toString()}
           horizontal={true}
+          ListHeaderComponent={books.length === 0 && !loading && <EmptyView />}
         />
       </Box>
     );
