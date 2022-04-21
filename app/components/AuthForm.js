ab290943450e2bbf1da897cb5c8cedcc98e2b0cf
António Gonçalves <acgtwentyone@gmail.com>
2022-04-21 08:39:15 -0100

    Update AuthForm.js

--- app/components/AuthForm.js
+++ app/components/AuthForm.js
@@ -16,6 +16,7 @@ import {StyleSheet} from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import auth from '@react-native-firebase/auth';
 import {yupResolver} from '@hookform/resolvers/yup';
+import {useNetInfo} from '@react-native-community/netinfo';
 
 import {Screen} from '.';
 import {FIREBASE_ERRORS, NAVIGATORS_NAME, ROUTES_NAME} from '../Utils';
@@ -36,42 +37,80 @@ const AuthForm = ({navigation, signin = true}) => {
     resolver: yupResolver(SigninSchema),
   });
   const {_showToastMsg} = useShowMessage();
+  const netInfo = useNetInfo();
 
   const navigateTab = () => {
     navigation.replace(NAVIGATORS_NAME.tab);
   };
 
+  const isInternet = () => {
+    return (
+      undefined !== netInfo &&
+      netInfo !== null &&
+      netInfo.isConnected !== null &&
+      netInfo.isConnected
+    );
+  };
+
+  const NoInternetAlert = () => {
+    const bgColor = useColorModeValue('black', 'white');
+    const textColor = useColorModeValue('white', 'black');
+    return (
+      <>
+        {!isInternet() && (
+          <Box
+            p={6}
+            backgroundColor={bgColor}
+            borderTopLeftRadius={32}
+            borderTopRightRadius={32}>
+            <Text color={textColor} fontSize={15} fontWeight="bold">
+              No internet connection. Please connect to internet first.
+            </Text>
+          </Box>
+        )}
+      </>
+    );
+  };
+
   const __processSiginOrSignup = async ({email, password}) => {
-    if (signin) {
+    if (isInternet()) {
+      if (signin) {
+        auth()
+          .signInWithEmailAndPassword(email, password)
+          .then(() => {
+            navigateTab();
+          })
+          .catch(error => {
+            _showToastMsg(FIREBASE_ERRORS[error.code]);
+          });
+      } else {
+        auth()
+          .createUserWithEmailAndPassword(email, password)
+          .then(() => {
+            navigateTab();
+          })
+          .catch(error => {
+            _showToastMsg(FIREBASE_ERRORS[error.code]);
+          });
+      }
+    } else {
+      _showToastMsg('No internet connection.');
+    }
+  };
+
+  const _signinAnonymously = () => {
+    if (isInternet()) {
       auth()
-        .signInWithEmailAndPassword(email, password)
+        .signInAnonymously()
         .then(() => {
-          navigateTab();
+          navigation.replace(NAVIGATORS_NAME.tab);
         })
         .catch(error => {
           _showToastMsg(FIREBASE_ERRORS[error.code]);
         });
     } else {
-      auth()
-        .createUserWithEmailAndPassword(email, password)
-        .then(() => {
-          navigateTab();
-        })
-        .catch(error => {
-          _showToastMsg(FIREBASE_ERRORS[error.code]);
-        });
+      _showToastMsg('No internet connection.');
     }
-  };
-
-  const _signinAnonymously = () => {
-    auth()
-      .signInAnonymously()
-      .then(() => {
-        navigation.replace(NAVIGATORS_NAME.tab);
-      })
-      .catch(error => {
-        _showToastMsg(FIREBASE_ERRORS[error.code]);
-      });
   };
 
   const ErrorMessage = ({name}) => (
@@ -185,6 +224,7 @@ const AuthForm = ({navigation, signin = true}) => {
           </HStack>
         </Box>
       </ScrollView>
+      <NoInternetAlert />
     </Screen>
   );
 };
