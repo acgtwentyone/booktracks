3e9b11e3463a37524a7f35dd30bd6b32a875c44e
António Gonçalves <acgtwentyone@gmail.com>
2022-06-23 22:21:34 -0100

    Update index.js

--- app/hooks/index.js
+++ app/hooks/index.js
@@ -1,7 +1,8 @@
+export {default as useAlertError} from './useAlertError';
+export {default as useErrorColor} from './useErrorColor';
 export {default as useIsLoggedin} from './useIsLoggedin';
 export {default as useLoadBooks} from './useLoadBooks';
 export {default as useLoadNotes} from './useLoadNotes';
 export {default as useTabScreenOptions} from './useTabScreenOptions';
 export {default as useScreenOptions} from './useScreenOptions';
 export {default as useShowMessage} from './useShowMessage';
-export {default as useAlertError} from './useAlertError';
