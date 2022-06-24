b6aded3f83d8257025af9337d6a609f6125d3458
António Gonçalves <acgtwentyone@gmail.com>
2022-06-23 23:46:34 -0100

    Update SubmitButton.js

--- app/components/SubmitButton.js
+++ app/components/SubmitButton.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import {Button} from 'native-base';
+import {Button, HStack, Text} from 'native-base';
+import {AppActivityIndicator} from './';
 
-const SubmitButton = ({style, props, title, size, handleSubmit, onSubmit}) => (
+const SubmitButton = ({
+  style,
+  props,
+  title,
+  size,
+  handleSubmit,
+  onSubmit,
+  progress,
+  showProgressIndicator,
+}) => (
   <Button
     size={size || 'lg'}
     {...props}
     onPress={handleSubmit(onSubmit)}
     m={4}
     shadow={2}>
-    {title}
+    <HStack alignItems="center">
+      {progress && showProgressIndicator && <AppActivityIndicator m={0} />}
+      <Text>{title}</Text>
+    </HStack>
   </Button>
 );
 
