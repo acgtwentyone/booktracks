3bfd81fd0da72c574785aa20da8f86389730df4c
António Gonçalves <acgtwentyone@gmail.com>
2022-07-02 20:14:14 -0100

    Update AppActivityIndicator.js

--- app/components/AppActivityIndicator.js
+++ app/components/AppActivityIndicator.js
@@ -5,11 +5,7 @@ import {View, useColorModeValue} from 'native-base';
 const AppActivityIndicator = ({bg, style, m}) => {
   const activityIndicatorBg = useColorModeValue('#FFF', '#000');
   return (
-    <View
-      bg={bg ? bg : activityIndicatorBg}
-      flex={1}
-      style={style}
-      m={m ? m : 5}>
+    <View bg={bg ? bg : activityIndicatorBg} flex={1} style={style}>
       <ActivityIndicator />
     </View>
   );
